dd08840c2a433baed1001baf17ecc7da65996193
shruthi-270706 <uligondashruthi@gmail.com>
2025-06-14 11:33:26 +0530

    projects

---
+++ 27.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,N1,N2;
+    printf(" ");
+    scanf("%d",&N1);
+    printf(" ");
+    scanf("%d",&N2);
+    for(i=1;i<=N1;i++){
+        for(j=1;j<=N2;j++){
+            printf(" 1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
