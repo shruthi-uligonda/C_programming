0dfd61084ff928692f738392d8583a2b8b3bd91a
shruthi-270706 <uligondashruthi@gmail.com>
2025-07-07 19:28:00 +0530

    projects

---
+++ ibp-08.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	float l,b,p;
+	printf("enter l,b values:");
+	scanf("%f %f",&l,&b);
+	p=2*(l+b);
+	printf("perimeter of rectangle:%f",p);
+	
+}
